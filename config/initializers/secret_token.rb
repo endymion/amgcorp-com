47905f7f6c773b5d815a3b9ef529abbfc0fa8faa
Ryan Alyn Porter <rap@endymion.com>
2010-09-16 11:38:13 -0400

    Initial commit to amgcorp-com.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmgcorpCom::Application.config.secret_token = '1f1319776b308f9971c3c8d04b8833c7266fb8fd47fd69738330ff03cabfc8ae5d9c9bdbabb01f63095fe7f726b5654cd9762a8a3411abcee9626b5ba13e3166'
