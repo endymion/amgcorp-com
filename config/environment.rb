2b49d56f03ab6761ed144f17ac44327d55efaa19
Ryan Alyn Porter <rap@endymion.com>
2010-09-27 10:42:45 -0400

    Deal gracefully with the absence of the config/environment.yml file.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 AmgcorpCom::Application.initialize!
 
-YAML::load_file(File.join(%w(config environment.yml))).each_pair{|key, value| ENV[key] = value }
+begin
+  YAML::load_file(File.join(%w(config environment.yml))).each_pair{|key, value| ENV[key] = value }
+rescue
+  # The environment variables config file will be missing in production.
+end
