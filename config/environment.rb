ec18af176325e2a19cb5fc4caebec97bcd57efb9
Ryan Alyn Porter <rap@endymion.com>
2010-09-27 10:34:37 -0400

    Added a config/environment.yml for Heroku configuration keys and values.  Not tracked by Git, to keep the passwords out of the source code.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 AmgcorpCom::Application.initialize!
+
+YAML::load_file(File.join(%w(config environment.yml))).each_pair{|key, value| ENV[key] = value }
