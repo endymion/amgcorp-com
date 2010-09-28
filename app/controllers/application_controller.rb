72669a825cb19d2294e9930563d6da30cff6976e
Ryan Alyn Porter <rap@endymion.com>
2010-09-28 14:52:42 -0400

    Added log feedback for ENV config settings.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 private
 
    def authenticate
+     logger.warn "Authentication, seeking username: \"#{ENV['ADMIN_USERNAME']}\" and password beginning with \"#{ENV['ADMIN_PASSWORD'][0,4]}\"."
       authenticate_or_request_with_http_basic do |id, password| 
           id == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
       end
